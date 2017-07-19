129ab2a0921c4dfe3c0d3c73889735be3e19ccf0
eliott enos <eliottenos@gmail.com>
2017-07-19 14:31:23 -0400

    projects
    now includes links to projects for github and deployed

---
+++ assets/scripts/ui.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// const store = require('../store.js')
+const resume = function () {
+  $('.logo4').on('click')
+  $('#resumeShow').modal('show')
+}
+
+module.exports = {
+  resume
+}
